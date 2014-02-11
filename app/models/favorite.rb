bac9843830a3066c85b8f74c7e7f0e60b5051fda
Edward Kang <eyk23@cornell.edu>
2014-02-11 00:41:02 -0500

    finished the method for getting the top favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -12,8 +12,18 @@ class Favorite < ActiveRecord::Base
     primary_key: :user_api_id
   )
   
-  def self.top(team, limit = TOP_LIMIT)
+  def self.top(team, limit = 10)
     # TODO: Given a team, return the top n users
+    teams_top_favorites = Favorite.top_favorites(team, limit)
+  
+    teams_top_favorites.map do |favorite|
+      User.find_by_user_api_id(favorite.user_api_id)
+    end
+  end
+  
+  def self.top_favorites(team, limit)
+    team_id = Team.find_by_nickname(team).team_api_id
     
+    Favorite.includes(:user_api_id).where(team_api_id: team_id).order('current_amount DESC').limit(limit)
   end
 end
